e3856d0f22dcd7bd33f19be9fabc278866dc299f
anklet1 <flekufelix@gmail.com>
2023-02-16 21:47:19 -0800

    Hello ,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
-/**
+/*
  * File: 4-puts.c
- * Auth: Felix Fleku 
+ * Auth: Felix Fleku
  */
+
 #include <stdio.h>
 
 /**
  * main - Prints "Programming is like building a multilingual
  *                puzzle, followed by a new line.
+ *
  * Return: Always 0.
  */
 int main(void)
