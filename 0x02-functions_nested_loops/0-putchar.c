9d5b9df8263536eabd92ba67c275cdbd42aad610
anklet1 <flekufelix@gmail.com>
2023-02-22 19:46:58 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,26 +2,25 @@
  * File : 0-putchar.c
  * Auth: Felix Fleku
  */
+#include "main.h"
 
-#include "main.c"
-
-/*main- prints putchar followed by a new line
- * Return: Always 0
- */
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
+
 {
-
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-
-
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
 }
