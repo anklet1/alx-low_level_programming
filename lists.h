a5a23cb486b9c7764288306d2f16d74a5de87aec
Fleku Felix <flekufelix@gmail.com>
2023-04-03 15:55:40 -0700

    updated

--- lists.h
+++ lists.h
@@ -7,7 +7,8 @@
  * Desc: Header file containing prototypes and definitions for all functions
  *       and types written in the 0x13  more_singly_linked_lists directory.
  */
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
