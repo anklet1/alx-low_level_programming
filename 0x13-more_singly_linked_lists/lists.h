619738be1e0e28009f9dba848a1cc36503fa0ffd
Fleku Felix <flekufelix@gmail.com>
2023-04-25 13:38:22 -0700

    Deleted File/IO directory

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,9 +4,20 @@
 /*
  * File: lists.h
  * Auth: Felix Fleku
- * Desc: Header file containing prototypes and definitions for all functions
+ * Desc: Header file containing prototypes  for all functions
  *       and types written in the 0x13  more_singly_linked_lists directory.
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  */
+#include <stddef.h>
+#include <stdio.h>
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -19,7 +30,6 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
+
 #endif
+
