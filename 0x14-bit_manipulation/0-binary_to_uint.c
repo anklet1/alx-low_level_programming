ad7610ae5ec17c0f7b5c9be819fbe009e49ddc3d
anklet1 <flekufelix@gmail.com>
2023-07-08 13:52:44 -0700

    Binary to interger

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
- * main- Entry point
- * binary_to_uint - Convert binary number to unsigned int
- * @b: String that contain binary number
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * Return: Returns the converted number
+ * Return: unsigned int.
  */
-int main(void)
-{ 	unsigned int binary_to_uint(const char *b);
-	unsigned int x;
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ui;
 	int len, base_two;
 
-	if (!b){
+	if (!b)
 		return (0);
-		}
-	x = 0;
+
+	ui = 0;
 
 	for (len = 0; b[len] != '\0'; len++)
 		;
-	{
 
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-		;
 	{
-	if (b[len] != '0' && b[len] != '1')
-	
-	return (0);
-	}
-	if (b[len] & 1)
-	
-		x += base_two;
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
 
-	return (x);
+	return (ui);
 }
