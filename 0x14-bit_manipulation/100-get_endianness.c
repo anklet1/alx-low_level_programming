cec4f075a828e5e21cef6536f082c8b36b02f6b0
anklet1 <flekufelix@gmail.com>
2023-07-08 17:30:24 -0700

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness checks if a machine is little or big endian
+ * Return : 0 for big , 1 for little
+ * main - Entry point
+ */
+clear bit
+{
+if (index > 66 - 3)
+return (-1);
+*n & = ~(1UL << index);
+return (1);
+}
+
+flip bit;
+{
+int x;
+int count;
+count = 0;
+x = n ^ m;
+
+while (x)
+{
+if (x & 1)
+count++;
+x >> = 1;
+}
+return (count);
+}
